bf63ffa549c18b1d891ddf1e034918e149ad0477
veronica.ramires <veronica_ramires@live.com>
2026-04-16 21:39:21 -0400

    Configuration Documentation API Swagger

--- src/main/java/com/project/apirest/api/ApiRestClient.java
+++ src/main/java/com/project/apirest/api/ApiRestClient.java
@@ -1,15 +1,12 @@
 package com.project.apirest.api;
+ import com.project.apirest.data.entity.Client;
+ import com.project.apirest.service.ClientService;
+ import org.springframework.data.domain.Page;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.web.bind.annotation.*;
 
-import com.project.apirest.data.entity.Client;
-import com.project.apirest.service.ClientService;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+ import java.util.List;
 
 @RestController
 @RequestMapping("/client")
--- src/main/java/com/project/apirest/api/ApiRestController.java
+++ src/main/java/com/project/apirest/api/ApiRestController.java
@@ -1,11 +1,11 @@
 package com.project.apirest.api;
 
+
 import com.project.apirest.service.RequisitionService;
-import org.hibernate.query.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController //quando tem interacao so front - o Controler so serve quando usa Thymeleaf
-@RequestMapping("requests")  //so para organizacao
+@RequestMapping("/requests")  //so para organizacao
 public class ApiRestController {
 
     private final RequisitionService service;  //pesquisar porque nao usar autoriade ...
